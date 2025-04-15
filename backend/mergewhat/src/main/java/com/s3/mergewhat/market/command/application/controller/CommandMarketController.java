88a0b9e1c66184b697d70b728a19d683802a2d4d
Haegwan-Choe <kkhagwan@naver.com>
2025-04-15 17:41:14 +0900

    webhook test2

--- backend/mergewhat/src/main/java/com/s3/mergewhat/market/command/application/controller/CommandMarketController.java
+++ backend/mergewhat/src/main/java/com/s3/mergewhat/market/command/application/controller/CommandMarketController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class CommandMarketController {
 
 //    private final CommandMarketService commandMarketService;
-    // webhook test
+    // webhook test2
 
 }
