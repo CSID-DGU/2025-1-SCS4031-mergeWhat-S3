5c4dded2cd1a670f1df8411ccf6c2d0b3645918a
Haegwan-Choe <kkhagwan@naver.com>
2025-04-20 21:07:55 +0900

    Feat: DDL 구문 수정 후 member 엔티티 추가 반영

--- backend/mergewhat/src/main/java/com/s3/mergewhat/member/command/domain/aggregate/entity/Member.java
+++ backend/mergewhat/src/main/java/com/s3/mergewhat/member/command/domain/aggregate/entity/Member.java
@@ -1,4 +1,48 @@
 package com.s3.mergewhat.member.command.domain.aggregate.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+@Table(name = "member")
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "nickname", nullable = false)
+    private String nickname;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "profile_url", nullable = true)
+    private String profileUrl;
+
+    @Column(name = "role", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(name = "biz_registration_number", nullable = true)
+    private String bizRegistrationNumber;
+
+    @Column(name = "biz_registration_url", nullable = true)
+    private String bizRegistrationUrl;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime cratedAt;
+
+
 }
---
+++ backend/mergewhat/src/main/java/com/s3/mergewhat/member/command/domain/aggregate/entity/Role.java
@@ -0,0 +1,6 @@
+package com.s3.mergewhat.member.command.domain.aggregate.entity;
+
+public enum Role {
+    USER,
+    MERCHANT
+}
